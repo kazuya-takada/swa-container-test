24822da7c75e52b387271e8ba81752fbd4161925
Kazuya Takada <kazuya-takada@scsac.co.jp>
2023-04-12 09:08:00 +0900

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { ChangeEvent, FormEvent, useState } from "react";
 import axios, { AxiosResponse } from "axios";
 
+// NEXT_PUBLIC_API_BASE_URLは/apiにする
 const BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
 
